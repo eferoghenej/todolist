4b795423c97d21d4206ffac4906139f6b671c531
Eferoghene Jarikre <eferoghenej@Eferoghenes-MacBook-Air.local>
2022-04-22 03:04:52 +0100

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -158,6 +158,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(port, function() {
+app.listen(process.env.PORT || port, function() {
   console.log("Server started on port 3000");
 });
